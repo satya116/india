a8f136cf95fe974f686f7c797623fd4a2d2bf5a9
satya <satya@extendtech.net>
2022-09-20 13:18:01 +0530

    lol

--- server.js
+++ server.js
@@ -3,3 +3,4 @@ console.log("2nd commit");
 console.log("Assam from master");
 console.log("Bongaigaon branch from Assam");
 console.log("Barpeta branch from Assam");
+console.log("Barpeta branch from Assam 2");
